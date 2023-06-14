022fa4582ab253a22b22129c62005fb58ec530ea
Sonia Heer <sonia@Sonias-MacBook-Air.local>
2023-06-14 10:02:18 +0100

    add a destination search box

---
+++ travel-planner-fe/app/destination-search/apis.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export function destinationSearch(search_text: string){
+    return axios.get(`https://api.mapbox.com/search/searchbox/v1/suggest?q=${search_text}&access_token=pk.eyJ1IjoibGFpOTYiLCJhIjoiY2xpdWVhdmQ3MHkybjNobzdnbjJwcmx6YSJ9.0CYohMf5CN77cD-BOo7mhw&session_token=ec1d4ed0-0a00-11ee-be56-0242ac120002`)
+        .then((response) => {
+            // console.log(response)
+            return response.data;
+        })
+}
