9c84f88a4c4cfdcc6dce472deb9bd0b28b90b71b
Laila Case <lailacase@Lailas-MacBook-Pro.local>
2023-06-15 14:15:41 +0100

    refactorstate

--- travel-planner-fe/components/destinationSearch.tsx
+++ travel-planner-fe/components/destinationSearch.tsx
@@ -1,55 +1,40 @@
-'use client'
+"use client";
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import { destinationSearch } from "./apis";
 import { SearchBox } from "@mapbox/search-js-react";
 
-export default function Destination({setCurrentPage}){
+export default function Destination({ setCurrentPage }) {
+  const [input, setInput] = React.useState("London");
 
-  const [ input, setInput ] = React.useState('london')
+  // useEffect(() => {
+  //   destinationSearch(input)
+  //     .then((response) => {
 
-  useEffect(() => {
-    destinationSearch(input)
-      .then((response) => {
-        
-    })
-  }, [])
-
+  //   })
+  // }, [])
 
   const handleSubmit = (e: React.FormEvent) => {
-    console.log(e.target[0].value)
-    setInput(e.target[0].value)
-    setCurrentPage((currPage) => currPage + 1 )
-    e.preventDefault()
-  }
+    setCurrentPage((currPage) => currPage + 1);
+    e.preventDefault();
+  };
+
+  const handleRetrieve = (e) => {
+    setInput(e.features[0].properties.name);
+  };
 
   return (
+    <form onSubmit={handleSubmit}>
+      <SearchBox
+        options={{ types: "place" }}
+        onRetrieve={handleRetrieve}
+        value={input}
+        accessToken={
+          "pk.eyJ1IjoibGFpOTYiLCJhIjoiY2xpdWVhdmQ3MHkybjNobzdnbjJwcmx6YSJ9.0CYohMf5CN77cD-BOo7mhw"
+        }
+      />
 
-   <form onSubmit={handleSubmit}>
-
-      <SearchBox value={input} accessToken={'pk.eyJ1IjoibGFpOTYiLCJhIjoiY2xpdWVhdmQ3MHkybjNobzdnbjJwcmx6YSJ9.0CYohMf5CN77cD-BOo7mhw'}/>
-      
-        <button type="submit">Continue</button>
+      <button type="submit">Continue</button>
     </form>
-  )
-
-   
-
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
