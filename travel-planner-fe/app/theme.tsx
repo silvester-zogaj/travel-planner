cb5a6a3e3c6b87659a5d07f29f6880e9c98db6a0
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2023-06-19 17:22:23 +0100

    dark/light

--- travel-planner-fe/app/theme.tsx
+++ travel-planner-fe/app/theme.tsx
@@ -2,7 +2,8 @@
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
 import NextLink from "next/link";
-import React, { ReactElement, ReactNode, forwardRef } from "react";
+import React, { ReactElement, ReactNode, forwardRef, useMemo } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const LinkBehaviour = forwardRef(function LinkBehaviour(props, ref) {
   // @ts-ignore
@@ -25,8 +26,18 @@ export const theme = createTheme({
   },
 });
 
-const makeTheme = ({ children }: { children: ReactNode }) => {
+const MakeTheme = ({ children }: { children: ReactNode }) => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
-export default makeTheme;
+export default MakeTheme;
