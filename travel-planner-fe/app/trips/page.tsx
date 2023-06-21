f06a85355ef5ab66401ff9664e922f24136fdf3d
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2023-06-21 12:18:56 +0100

    merge conflicts

--- travel-planner-fe/app/trips/page.tsx
+++ travel-planner-fe/app/trips/page.tsx
@@ -1,16 +1,92 @@
 import styles from "../page.module.css";
 import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import {
+  fetchDataFromFirebase,
+  deleteDataFromFirebase,
+} from "@/utils/firebaseUtils";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Typography from "@mui/material/Typography";
+import LoadingPage from "@/components/loadingPage";
+import { AuthContext } from "@/app/context/AuthContext";
 
 export default function Trips() {
+  const [trips, setTrips] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!user?.uid) return;
+
+      try {
+        setIsLoading(true);
+        const data = await fetchDataFromFirebase(db, user.uid);
+        const tripNames = Object.keys(data);
+        setTrips(tripNames);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [user?.uid]);
+
+  const handleDelete = async (destination: string) => {
+    if (!user?.uid) return;
+
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this itinerary?"
+    );
+    if (!confirmDelete) return;
+
+    await deleteDataFromFirebase(db, user?.uid, destination);
+    setTrips((prevTrips) => prevTrips.filter((trip) => trip !== destination));
+  };
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
+  if (trips.length === 0) {
+    return <p>No trips found</p>;
+  }
+
   return (
     <main className={styles.tripList}>
-      <h1>Your trips 🌎</h1>
-      <Link href="/itineraries">
-        <button className={styles.trip}>Barcelona 🗑️ </button>
-      </Link>
-      <button className={styles.trip}>Paris 🗑️</button>
-      <button className={styles.trip}>Tokyo 🗑️</button>
-      <button className={styles.trip}>New York 🗑️</button>
+      <Stack gap={1}>
+        <Typography textAlign="center" variant="h3" fontWeight="bold">
+          Your trips 🌎
+        </Typography>
+        {trips.map((trip, i) => (
+          <ButtonGroup variant="contained" key={i}>
+            <Button
+              href="/itineraries"
+              component={Link}
+              size="large"
+              sx={{
+                width: "100%",
+                height: "80px",
+              }}
+            >
+              <Typography fontWeight="bold">{trip}</Typography>
+            </Button>
+            <Button
+              onClick={() => {
+                handleDelete(trip);
+              }}
+              size="large"
+            >
+              <DeleteIcon sx={{ color: "red" }} />
+            </Button>
+          </ButtonGroup>
+        ))}
+      </Stack>
     </main>
   );
 }
